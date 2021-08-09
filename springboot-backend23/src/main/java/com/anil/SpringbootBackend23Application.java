5ebce2a179acecc21a08f7f8c1f8d3af5bc5a0e6
asharma <anilsandhya@gmail.com>
2021-08-08 21:35:02 -0400

    first commit

---
+++ springboot-backend23/src/main/java/com/anil/SpringbootBackend23Application.java
@@ -0,0 +1,13 @@
+package com.anil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackend23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackend23Application.class, args);
+	}
+
+}
